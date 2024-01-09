567e2be1b585aaf006518d53804631eadfab6447
Rafael Melo de Almeida <meloalmeidarafa@gmail.com>
2024-01-09 20:29:52 -0300

    subindo o site

---
+++ main.js
@@ -0,0 +1,11 @@
+let menu = document.query.querySelector('#menu-icon');
+let navbar = document.query.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
